1fc9de11ec9fe3f5b46e697133c670529dc5e6c8
mohdarshikhan <khanmohdarshi@gmail.com>
2021-10-06 13:34:07 +0530

    Style Layout, OnBoarding with three screens(Fresh Food, Fast Delivery, Easy Payment)

--- app/src/main/java/com/mak/navigationarchitecturedemo/ui/onboarding/screens/SecondScreen.kt
+++ app/src/main/java/com/mak/navigationarchitecturedemo/ui/onboarding/screens/SecondScreen.kt
@@ -1,8 +1,44 @@
 package com.mak.navigationarchitecturedemo.ui.onboarding.screens
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.mak.navigationarchitecturedemo.R
+import com.mak.navigationarchitecturedemo.databinding.FragmentSecondScreenBinding
 
 class SecondScreen : Fragment() {
 
+    private var _binding: FragmentSecondScreenBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+    private var viewPager: ViewPager2? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSecondScreenBinding.inflate(inflater, container, false)
+
+        viewPager = activity?.findViewById(R.id.viewPager)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListener()
+    }
+
+    private fun setListener() {
+        _binding?.btnNext?.setOnClickListener {
+            viewPager?.currentItem = 2
+        }
+    }
 
 }
--- app/src/main/java/com/mak/navigationarchitecturedemo/ui/onboarding/screens/ThirdScreen.kt
+++ app/src/main/java/com/mak/navigationarchitecturedemo/ui/onboarding/screens/ThirdScreen.kt
@@ -1,8 +1,49 @@
 package com.mak.navigationarchitecturedemo.ui.onboarding.screens
 
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.mak.navigationarchitecturedemo.R
+import com.mak.navigationarchitecturedemo.databinding.FragmentThirdScreenBinding
 
 class ThirdScreen : Fragment() {
 
+    private var _binding: FragmentThirdScreenBinding? = null
 
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentThirdScreenBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListener()
+    }
+
+    private fun setListener() {
+        _binding?.btnFinish?.setOnClickListener {
+            findNavController().navigate(R.id.action_viewPagerFragment_to_homeFragment)
+            onBoardingFinished()
+        }
+    }
+
+    private fun onBoardingFinished() {
+        val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putBoolean("Finished", true)
+        editor.apply()
+    }
 }
